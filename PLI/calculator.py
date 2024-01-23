6226da316f125f8504d3d24241d34f15f4bf759d
iach526526 <iach526526@gmail.com>
2024-01-23 22:00:37 +0800

    del comment

--- PLI/calculator.py
+++ PLI/calculator.py
@@ -4,9 +4,3 @@ import base64
 def calculate(x):
     random.seed(48763)
     return int(eval(x))
-# class Table():
-#     def __init__(self):
-#         Table=
-# x="NjU1ICogMzI4ICsgKDMzMyAtICg4NjgpICsgKDEpIC8gKDU1NikpIC8gMzE4IC0gNTU1IC0gKDc5OSAvICg5NjUpKSAvIDE1MCAvIDk2ICogNTIyICogNDU4"
-# print(base64.b64decode(x).decode('utf-8'))
-# print(calculate(base64.b64decode(x).decode('utf-8')))
