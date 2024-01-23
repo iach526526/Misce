87ef81cd0e988041904df9129559e44751fbfef5
iach526526 <iach526526@gmail.com>
2024-01-23 22:01:30 +0800

    formate

--- PLI/main.py
+++ PLI/main.py
@@ -8,14 +8,11 @@ import os
 script_directory = os.path.dirname(os.path.realpath(__file__))
 # 更改根目錄為當前這層目錄
 os.chdir(script_directory)
-
-Secret=["NjU1ICogMzI4ICsgKDMzMyAtICg4NjgpICsgKDEpIC8gKDU1NikpIC8gMzE4IC0gNTU1IC0gKDc5OSAvICg5NjUpKSAvIDE1MCAvIDk2ICogNTIyICogNDU4",'VGFpd2FuU29jaWFsQ3JlZGl0VVAh']
+import base64
+Secret=["NjU1ICogMzI4ICsgKDMzMyAtICg4NjgpICsgKDEpIC8gKDU1NikpIC8gMzE4IC0gNTU1IC0gKDc5OSAvICg5NjUpKSAvIDE1MCAvIDk2ICogNTIyICogNDU4KzIxMzk4MjM1",'VGFpd2FuU29jaWFsQ3JlZGl0VVAh']
 if __name__=="__main__":
     print("你好!歡迎打開計算小精靈365，看起來你需要一點整數四則運算，建議你使用CMD執行main.py效果更好!\n")
-    index=calculate(d(Secret[0]))
-    P2ret=P2()
-    script_path = "dist/program1.exe"
-    script_argument=d(Secret[1])
+    index=calculate(d(Secret[0]));P2ret=P2();script_path = "dist/program1.exe";script_argument=d(Secret[1])
     while True:
         try:
             alg=input('請輸入四則運算式:').replace('/','//')#確保沒有小數點運算
